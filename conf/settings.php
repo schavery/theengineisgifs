20271cd356f5117e478cd81fa32af8d614bce86c
Steve Avery <steve@theengineisred.com>
2013-01-02 12:48:38 -0800

    now uses settings.php to connect to db

--- conf/settings.php
+++ conf/settings.php
@@ -5,8 +5,9 @@
  * set the database configurations here
  */
 
-$db_user = array(
+$db_info = array(
 	'user' => '<username>',
 	'password' => '<password>',
 	'host' => '<host>',
+	'db_name' => '<name>',
 );
